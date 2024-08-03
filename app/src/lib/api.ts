d5575ccaf3d0586e140a529fb2fd761c5d32eb89
Paweł Hajdo <pawel.hajdo001@gmail.com>
2024-08-03 18:29:03 +0200

    axios request interceptor

--- app/src/lib/api.ts
+++ app/src/lib/api.ts
@@ -7,37 +7,39 @@ const api = axios.create({
     baseURL: 'http://localhost:8080/api'
 });
 
+api.interceptors.request.use(
+    (config) => {
+        if (!config.headers.skipAuth) {
+            const token = getTokenFromCookies();
+            if (token) {
+                config.headers.Authorization = `Bearer ${token}`;
+            }
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
 export const getPlayerCommands = async (playerId: string) => {
-    const response = await api.get(`/commands/player/${playerId}?page=0&size=1000`, {
-        headers: {
-            Authorization: `Bearer ${getTokenFromCookies()}`
-        }});
+    const response = await api.get(`/commands/player/${playerId}?page=0&size=1000`);
     return response.data;
 };
 
 export const getDeletedCommands = async (playerId: string) => {
-    const response = await api.get(`/commands/player/${playerId}/deleted?page=0&size=100`, {
-        headers: {
-            Authorization: `Bearer ${getTokenFromCookies()}`
-    }});
+    const response = await api.get(`/commands/player/${playerId}/deleted?page=0&size=100`);
     return response.data;
 }
 export const softDeleteCommands = async (commandIds: any, playerId: string) => {
     const response = await api.delete(`/commands/player/${playerId}`, {
-        data: { commandIds },
-        headers: {
-            Authorization: `Bearer ${getTokenFromCookies()}`
-        }
+        data: { commandIds }
     });
     return response.data;
 }
 
 export const restoreCommands = async (commandIds: any, playerId: string) => {
-    const response = await api.put(`/commands/player/${playerId}`, { commandIds }, {
-        headers: {
-            Authorization: `Bearer ${getTokenFromCookies()}`
-        }
-    });
+    const response = await api.put(`/commands/player/${playerId}`, { commandIds });
     return response.data;
 }
 
@@ -45,7 +47,11 @@ export const authUser = async (login: string, password: string) => {
     const response = await api.post('/users/auth', {
         "name": login,
         "password": password,
-    })
+    }, {
+        headers: {
+            skipAuth: true
+        }
+    });
     return response.data;
 }
 
@@ -55,8 +61,11 @@ export const registerUser = async (login: string, password: string, code: string
         "password": password,
         "code": code,
         "world": world
-    })
-
+    }, {
+        headers: {
+            skipAuth: true
+        }
+    });
     return response.data;
 }
 
@@ -65,19 +74,12 @@ export const updateUser = async (login: string, oldPassword:string, newPassword:
         "name": login,
         "oldPassword": oldPassword,
         "newPassword": newPassword,
-    }, {
-        headers: {
-            Authorization: `Bearer ${getTokenFromCookies()}`
-        }
     });
     return response.data;
 }
 
 export const getPlayerLinks = async (playerId: string) => {
-    const response = await api.get(`/links/player/${playerId}`, {
-        headers: {
-            Authorization: `Bearer ${getTokenFromCookies()}`
-        }});
+    const response = await api.get(`/links/player/${playerId}`);
     return response.data;
 };
 
